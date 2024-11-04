7c937cc46dfb2b4b1230f6fbe8c5cab3616674e5
taliyameyswara <taliyameyswara@gmail.com>
2024-11-04 20:34:25 +0700

    🐞 fix: directory

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const Home = () => {
   const [scrollY, setScrollY] = useState(0);
